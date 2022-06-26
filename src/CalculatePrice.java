19844ce923cc258dd6f9667157f3494c07c732f3
richalljangi <richall-jangi@hotmail.com>
2022-06-26 02:02:50 +0200

    Keep Unit Interfaces Small voor better codehub

--- src/CalculatePrice.java
+++ src/CalculatePrice.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class CalculatePrice {
     public static  Double getTotalOrdercost(double orderCost, double shippingCost) {
         double totalOrderCost;
@@ -19,15 +21,26 @@ public class CalculatePrice {
 
 
 
-    public static Double getCalculateBtw(double totalOrderCost, double orderCost, double shippingCost, double importfee, double btw) {
-        double btwCost;
-        if(totalOrderCost > 150){
-            btwCost = (orderCost + shippingCost) * (importfee+ 1) * btw;
-        }else{
-          btwCost = 0.0;
+//    public static Double getCalculateBtw(double totalOrderCost, double orderCost, double shippingCost, double importfee, double btw) {
+//        double btwCost;
+//        if(totalOrderCost > 150){
+//            btwCost = (orderCost + shippingCost) * (importfee+ 1) * btw;
+//        }else{
+//          btwCost = 0.0;
+//        }
+//
+//        return btwCost;
+//    }
+
+
+
+    public static Double getCalculateBtw(HashMap<String, Double> hm) {
+        if(hm.get("totalOrderCost") > 150){
+            return(hm.get("orderCost") + hm.get("shippingCost") * (hm.get("importfee")+ 1) * hm.get("btw"));
+
         }
 
-        return btwCost;
+        return 0.0;
     }
 
 
@@ -42,5 +55,12 @@ public class CalculatePrice {
         return totalImportfeeCost;
     }
 
+    public static void Hashmap(){
+        HashMap<String, Double> hm = new HashMap<String, Double>();
+        hm.put("btw", 21.0);
+        hm.put("kosten", 100.0);
+        System.out.println(hm);
+
+    }
 
 }
